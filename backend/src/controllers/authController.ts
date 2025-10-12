2ae42404ef4c58519ce10b329f03a44ed26a0e35
Xheenkhalil <engrzyfer@gmail.com>
2025-10-12 01:29:55 +0100

    Added a lax instead of strict in the auth controller

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -72,7 +72,7 @@ export const loginUser = async (req: Request, res: Response) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
