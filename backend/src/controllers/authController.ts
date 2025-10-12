16231f0bfecb84e6ea3df72366e56ed72c1229c0
Xheenkhalil <engrzyfer@gmail.com>
2025-10-12 02:29:11 +0100

    stable version

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -71,8 +71,8 @@ export const loginUser = async (req: Request, res: Response) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
